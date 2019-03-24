46a3d1712d5cbd99655a20309296019ebe22d983
Paddy McGlone <paddymcglone@gmail.com>
2019-03-24 13:00:12 +0000

    Updating the index.cshtml.cs file, searching for genres.

--- RazorPagesMovies/RazorPagesMovies/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovies/RazorPagesMovies/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovies.Models;
 
@@ -19,18 +20,26 @@ namespace RazorPagesMovies.Pages.Movies
         }
 
         public IList<Models.Movies> Movies { get; set; }
+        public SelectList Genres;
+        public string MovieGenre { get; set; }
 
         // The new 'Search' approach
         public async Task OnGetAsync(string searchString)
         {
+            IQueryable<string> genresQuery = from m in _context.Movies
+                                             orderby m.Genre
+                                             select m.Genre;
+
             var movies = from m in _context.Movies
                          select m;
 
             if (!String.IsNullOrEmpty(searchString))
-            {
                 movies = movies.Where(m => m.Title.Contains(searchString));
-            }
 
+            if (!String.IsNullOrEmpty(MovieGenre))
+                movies = movies.Where(m => m.Genre == MovieGenre);
+
+            Genres = new SelectList(await genresQuery.Distinct().ToListAsync());
             Movies = await movies.ToListAsync();
         }
 
