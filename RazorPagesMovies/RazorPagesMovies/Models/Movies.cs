7874831404eb826147d06265a389feba14f93b0d
Paddy McGlone <paddymcglone@gmail.com>
2019-03-24 11:31:23 +0000

    Adding the data model.

---
+++ RazorPagesMovies/RazorPagesMovies/Models/Movies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovies.Models
+{
+    public class Movies
+    {
+        #region Properties
+
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public Decimal Price { get; set; }
+
+        #endregion
+    }
+}
